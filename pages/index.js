a10c12fea860c9fa90aeb3a2de0b3fd939068b32
musebe <emusebe9@gmail.com>
2022-03-28 13:12:48 +0300

    tensor flow complete addition

---
+++ pages/index.js
@@ -0,0 +1,236 @@
+import React, { useRef, useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import { Button, Card, Container, Chip } from "@material-ui/core";
+
+const modelConfig = {
+  architecture: "MobileNetV1",
+  outputStride: 16,
+  multiplier: 1,
+  quantBytes: 4,
+};
+
+export default function Home() {
+  let ctx_out, video_in, ctx_tmp, c_tmp, c_out;
+
+  const processedVid = useRef();
+  const rawVideo = useRef();
+  const startBtn = useRef();
+  const closeBtn = useRef();
+  const videoDownloadRef = useRef();
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    if (model) return;
+    const start_time = Date.now() / 1000;
+
+    bodyPix.load(modelConfig).then((m) => {
+      setModel(m);
+      const end_time = Date.now() / 1000;
+      console.log(`model loaded successfully, ${end_time - start_time}`);
+    });
+  }, []);
+
+  const segmentationConfig = {
+    internalResolution: "full",
+    segmentationThreshold: 0.1,
+    scoreThreshold: 0.4,
+    flipHorizontal: true,
+    maxDetections: 1,
+  };
+
+  let recordedChunks = [];
+  let localStream = null;
+  let options = { mimeType: "video/webm; codecs=vp9" };
+  let mediaRecorder = null;
+  let videoUrl = null;
+
+  const startCamHandler = async () => {
+    console.log("Starting webcam and mic ..... ");
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+
+    // console.log(model);
+
+    //populate video element
+    rawVideo.current.srcObject = localStream;
+    video_in = rawVideo.current;
+    rawVideo.current.addEventListener("loadeddata", (ev) => {
+      console.log("loaded data.");
+      transform();
+    });
+
+    mediaRecorder = new MediaRecorder(localStream, options);
+    mediaRecorder.ondataavailable = (event) => {
+      console.log("data-available");
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+      }
+    };
+    mediaRecorder.start();
+  };
+
+  const stopCamHandler = () => {
+    console.log("Hanging up the call ...");
+    localStream.getTracks().forEach((track) => track.stop());
+
+    mediaRecorder.onstop = async (event) => {
+      let blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+
+      // Save original video to cloudinary
+      await readFile(blob).then((encoded_file) => {
+        uploadVideo(encoded_file);
+      });
+
+      videoDownloadRef.current.href = URL.createObjectURL(blob);
+      videoDownloadRef.current.download =
+        new Date().getTime() + "-locastream.webm";
+    };
+  };
+
+  function readFile(file) {
+    console.log("readFile()=>", file);
+    return new Promise(function (resolve, reject) {
+      let fr = new FileReader();
+
+      fr.onload = function () {
+        resolve(fr.result);
+      };
+
+      fr.onerror = function () {
+        reject(fr);
+      };
+
+      fr.readAsDataURL(file);
+    });
+  }
+
+  const uploadVideo = async (base64) => {
+    console.log("uploading to backend...");
+    try {
+      fetch("/api/upload", {
+        method: "POST",
+        body: JSON.stringify({ data: base64 }),
+        headers: { "Content-Type": "application/json" },
+      }).then((response) => {
+        console.log("successfull session", response.status);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  let transform = () => {
+    c_out = processedVid.current;
+    ctx_out = c_out.getContext("2d");
+
+    c_tmp = document.createElement("canvas");
+    c_tmp.setAttribute("width", 800);
+    c_tmp.setAttribute("height", 450);
+
+    ctx_tmp = c_tmp.getContext("2d");
+
+    computeFrame();
+  };
+
+  let computeFrame = async () => {
+    ctx_tmp.drawImage(
+      video_in,
+      0,
+      0,
+      video_in.videoWidth,
+      video_in.videoHeight
+    );
+
+    let frame = ctx_tmp.getImageData(
+      0,
+      0,
+      video_in.videoWidth,
+      video_in.videoHeight
+    );
+
+    const { data: segmentation } = await model.segmentPerson(
+      frame,
+      segmentationConfig
+    );
+
+    // .then((segmentation) => {
+    let output_img = ctx_out.getImageData(
+      0,
+      0,
+      video_in.videoWidth,
+      video_in.videoHeight
+    );
+
+    for (let i = 0; i < segmentation.length; i++) {
+      // Extract data into r, g, b, a from imgData
+      const [r, g, b, a] = [
+        frame.data[i * 4],
+        frame.data[i * 4 + 1],
+        frame.data[i * 4 + 2],
+        frame.data[i * 4 + 3],
+      ];
+
+      // Calculate the gray color
+      const gray = 0.3 * r + 0.59 * g + 0.11 * b;
+
+      // Set new RGB color to gray if map value is not 1
+      // for the current pixel in iteration
+      [
+        output_img.data[i * 4],
+        output_img.data[i * 4 + 1],
+        output_img.data[i * 4 + 2],
+        output_img.data[i * 4 + 3],
+      ] = !segmentation[i] ? [gray, gray, gray, 255] : [r, g, b, a];
+    }
+
+    ctx_out.putImageData(output_img, 0, 0);
+    setTimeout(computeFrame, 0);
+  };
+
+  return (
+    <div className="container">
+      {model && (
+        <>
+          <div className="card">
+            <div className="videos">
+              <video
+                className="display"
+                width={800}
+                height={450}
+                ref={rawVideo}
+                autoPlay
+                playsInline
+              ></video>
+            </div>
+
+            <canvas
+              className="display"
+              width={800}
+              height={450}
+              ref={processedVid}
+            ></canvas>
+          </div>
+          <div className="buttons">
+            <button className="button" onClick={startCamHandler} ref={startBtn}>
+              Start Webcam
+            </button>
+            <button className="button" onClick={stopCamHandler} ref={closeBtn}>
+              Close and upload original video
+            </button>
+            <button className="button">
+              <a ref={videoDownloadRef} href={videoUrl}>
+                Get Original video
+              </a>
+            </button>
+          </div>
+        </>
+      )}
+      {!model && <div>Loading machine learning models...</div>}
+    </div>
+  );
+}
